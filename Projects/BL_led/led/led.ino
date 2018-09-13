64de7192c61431691325f353233bf66e8e3c9a00
Vishwanath <vishwanathmalakar@gmail.com>
2018-09-13 23:43:25 +0530

    initial add

---
+++ Projects/BL_led/led/led.ino
@@ -0,0 +1,77 @@
+/*************************************************************
+  Download latest Blynk library here:
+    https://github.com/blynkkk/blynk-library/releases/latest
+
+  Blynk is a platform with iOS and Android apps to control
+  Arduino, Raspberry Pi and the likes over the Internet.
+  You can easily build graphic interfaces for all your
+  projects by simply dragging and dropping widgets.
+
+    Downloads, docs, tutorials: http://www.blynk.cc
+    Sketch generator:           http://examples.blynk.cc
+    Blynk community:            http://community.blynk.cc
+    Follow us:                  http://www.fb.com/blynkapp
+                                http://twitter.com/blynk_app
+
+  Blynk library is licensed under MIT license
+  This example code is in public domain.
+
+ *************************************************************
+
+  Blynk using a LED widget on your phone!
+
+  App project setup:
+    LED widget on V1
+ *************************************************************/
+
+/* Comment this out to disable prints and save space */
+#define BLYNK_PRINT Serial
+
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "cbdbb8e5d10a4d2e821a0e24ae4f08b3";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "S-404";
+char pass[] = "123456789a";
+
+WidgetLED led1(D1);
+
+BlynkTimer timer;
+
+// V1 LED Widget is blinking
+void blinkLedWidget()
+{
+  if (led1.getValue()) {
+    led1.off();
+    Serial.println("LED on V1: off");
+  } else {
+    led1.on();
+    Serial.println("LED on V1: on");
+  }
+}
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+
+  Blynk.begin(auth, ssid, pass);
+  // You can also specify server:
+  //Blynk.begin(auth, ssid, pass, "blynk-cloud.com", 80);
+  //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8080);
+
+  timer.setInterval(1000L, blinkLedWidget);
+}
+
+void loop()
+{
+  Blynk.run();
+  timer.run();
+}
+
